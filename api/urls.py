f5d9d531a2b1b8622f21d54d370dc97f42d282c2
igornieb <igornieb@gmail.com>
2023-09-20 20:34:49 +0200

    added token auth

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from api.views import *
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
     path('pictures', PictureList.as_view(), name='picture-list'),
@@ -7,4 +9,6 @@ urlpatterns = [
     path('picture/<uuid:pk>', PictureDetails.as_view(), name='picture-details'),
     path('picture/<uuid:pk>/<int:height>', PictureDetails.as_view(), name='picture-details'),
     path('picture-time-link/<uuid:pk>', TimePictureDetails.as_view(), name='picture-time-link'),
+    path('token/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 ]
