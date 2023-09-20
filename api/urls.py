003cb963eebff2cdc9bb849fd1754d111f2c2e20
igornieb <igornieb@gmail.com>
2023-09-20 19:26:34 +0200

    added api urls

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,8 @@ from api.views import *
 
 urlpatterns = [
     path('pictures', PictureList.as_view(), name='picture-list'),
+    path('pictures/shared', TimePictureList.as_view(), name='shared-picture-list'),
+    path('picture/<uuid:pk>', PictureDetails.as_view(), name='picture-details'),
+    path('picture/<uuid:pk>/<int:height>', PictureDetails.as_view(), name='picture-details'),
+    path('picture-time-link/<uuid:pk>', TimePictureDetails.as_view(), name='picture-time-link'),
 ]
