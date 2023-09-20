440ee98300b27420e0c8acd0d674b22b5ce05366
igornieb <igornieb@gmail.com>
2023-09-20 21:46:04 +0200

    updated celery settings

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
-celery -A storagetiers beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler --detach
+celery -A imagestorage beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler --detach
 exec "$@"
