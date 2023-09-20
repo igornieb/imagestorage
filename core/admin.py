371e408174aecf0d3b09c5256cf5ff371ed69c93
igornieb <igornieb@gmail.com>
2023-09-20 15:04:05 +0200

    added models to adminpanel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from core.models import *
 
-# Register your models here.
+
+@admin.register(Tier)
+class TierAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('user', 'tier',)
+
+
+@admin.register(Picture)
+class PictureAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'owner',)
+
+
+@admin.register(TimeExpiringPicture)
+class TimePictureAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'picture', 'expires')
+
+
+@admin.register(FileHeight)
+class FileHorizontalHeight(admin.ModelAdmin):
+    list_display = ('size',)
