aae8468c33d2a0921dbb1e38e2cf3bee54f10e68
igornieb <igornieb@gmail.com>
2023-09-20 16:07:20 +0200

    added serializers for models from core

---
+++ api/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from core.models import Picture, TimeExpiringPicture
+
+
+class PictureSerializer(serializers.ModelSerializer):
+    urls = serializers.URLField(source='get_absolute_url', read_only=True)
+
+    class Meta:
+        model = Picture
+        fields = ['owner', 'name', 'urls']
+
+
+class TimePictureSerializer(serializers.ModelSerializer):
+    url = serializers.URLField(source='get_absolute_url', read_only=True)
+    picture = PictureSerializer(read_only=True)
+
+    class Meta:
+        model = TimeExpiringPicture
+        fields = ['url', 'picture', 'time']
